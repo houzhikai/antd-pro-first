857a11505d985836ed93471ce5a333545bad14bd
hzk <1562206787@qq.com>
2023-04-07 17:41:42 +0800

    fix: 测试antd 组件中英文

--- src/pages/FU/blocks/FuNav.tsx
+++ src/pages/FU/blocks/FuNav.tsx
@@ -1,14 +1,24 @@
 import MyLogo from '@/components/MyLogo';
 import FULogo from '@/icon/FULogo.svg';
-import { Button } from 'antd';
+import { Button, DatePicker } from 'antd';
+import type { DatePickerProps } from 'antd';
+
 import { useIntl } from 'umi';
 
 const FuNav = () => {
   const { formatMessage } = useIntl();
+
+  const onChange: DatePickerProps['onChange'] = (
+    date: any,
+    dateString: any,
+  ) => {
+    console.log(date, dateString);
+  };
   return (
     <div>
       <MyLogo src={FULogo} />
       <Button type="primary">{formatMessage({ id: 'fu.button' })}</Button>
+      <DatePicker onChange={onChange} picker="week" />
     </div>
   );
 };
