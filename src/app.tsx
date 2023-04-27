c87fe862e37106e4fd749211ae977324e0aa0160
hzk <1562206787@qq.com>
2023-04-27 10:18:35 +0800

    fix: 添加access的权限

--- src/app.tsx
+++ src/app.tsx
@@ -13,8 +13,10 @@ import '@/styles/index.less';
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
 // 更多信息见文档：https://next.umijs.org/docs/api/runtime-config#getinitialstate
-// export async function getInitialState(): Promise<{ name: string }> {
-//   return { name: '111' };
+export async function getInitialState(): Promise<{ name: string }> {
+  return { name: 'dontHaveAccess' };
+}
+
 export const layout = () => {
   const [state, setState] = useUrlState({ locale: '', theme: '' });
   let params = location.hash.split('?')[1] || '';
