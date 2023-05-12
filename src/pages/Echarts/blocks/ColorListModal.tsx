f5a9c5fbb7e2edfb8916c7b418c649408f223a84
hzk <1562206787@qq.com>
2023-05-12 08:44:33 +0800

    fix: 修改modal footer按钮

--- src/pages/Echarts/blocks/ColorListModal.tsx
+++ src/pages/Echarts/blocks/ColorListModal.tsx
@@ -1,18 +1,18 @@
 import { useModel } from '@umijs/max';
-import { Modal, Tooltip } from 'antd';
+import { Button, Modal, Tooltip } from 'antd';
 
-function ColorListModal() {
+const ColorListModal = () => {
   const { isShowModal, setIsShowModal, setChangeColor } =
     useModel('useEchartsModel');
 
-  const handleOk = () => {
-    setIsShowModal((obj) => {
-      return {
-        ...obj,
-        isOpen: false,
-      };
-    });
-  };
+  const colorList = [
+    { name: '科技蓝', color: '#1677ff' },
+    { name: '明清', color: '#13c2c2' },
+    { name: '日暮', color: '#faad14' },
+    { name: '火山', color: '#f6531c' },
+    { name: '暗绿', color: '#84af9b' },
+    { name: '灰色', color: '#d0d0d0' },
+  ];
 
   const handleCancel = () => {
     setIsShowModal((obj) => {
@@ -22,14 +22,7 @@ function ColorListModal() {
       };
     });
   };
-  const colorList = [
-    { name: '科技蓝', color: '#1677ff' },
-    { name: '明清', color: '#13c2c2' },
-    { name: '日暮', color: '#faad14' },
-    { name: '火山', color: '#f6531c' },
-    { name: '暗绿', color: '#84af9b' },
-    { name: '灰色', color: '#d0d0d0' },
-  ];
+
   const handleSelectColor = (color: string) => {
     setChangeColor((pre) => {
       let newArr = [...pre];
@@ -42,8 +35,11 @@ function ColorListModal() {
     <Modal
       title="选择想要更改的颜色"
       open={isShowModal.isOpen}
-      onOk={handleOk}
-      onCancel={handleCancel}
+      footer={[
+        <Button key="cancel" type="primary" onClick={handleCancel}>
+          关闭
+        </Button>,
+      ]}
     >
       <div style={{ display: 'flex', marginTop: 20 }}>
         {colorList.map((item) => (
@@ -63,6 +59,6 @@ function ColorListModal() {
       </div>
     </Modal>
   );
-}
+};
 
 export default ColorListModal;
