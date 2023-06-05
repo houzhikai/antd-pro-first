dc6d51e0d82247aa07c5279efb46f71bbb4e2ceb
hzk <1562206787@qq.com>
2023-06-05 20:10:55 +0800

    fix: change Test-flow page show

--- src/pages/TestFlow/index.tsx
+++ src/pages/TestFlow/index.tsx
@@ -1,50 +1,16 @@
-import { useCallback } from 'react';
-import ReactFlow, {
-  Node,
-  addEdge,
-  Background,
-  Edge,
-  Connection,
-  useNodesState,
-  useEdgesState,
-} from 'reactflow';
-
-import CustomNode from './blocks/components/CustomNode';
-
+import React, { useCallback } from 'react';
+import ReactFlow, { useNodesState, useEdgesState, addEdge } from 'reactflow';
 import 'reactflow/dist/style.css';
 
-const initialNodes: Node[] = [
-  {
-    id: '1',
-    type: 'input',
-    data: { label: 'Node 1' },
-    position: { x: 250, y: 5 },
-  },
-  { id: '2', data: { label: 'Node 2' }, position: { x: 100, y: 100 } },
-  { id: '3', data: { label: 'Node 3' }, position: { x: 400, y: 100 } },
-  {
-    id: '4',
-    type: 'custom',
-    data: { label: 'Custom Node' },
-    position: { x: 400, y: 200 },
-  },
-];
+import initialNodes from './blocks/components/nodes';
+import initialEdges from './blocks/components/edges';
 
-const initialEdges: Edge[] = [
-  { id: 'e1-2', source: '1', target: '2', animated: true },
-  { id: 'e1-3', source: '1', target: '3' },
-];
-
-const nodeTypes = {
-  custom: CustomNode,
-};
-
-const BasicFlow = () => {
+const TestFlow = () => {
   const [nodes, , onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
   const onConnect = useCallback(
-    (params: Edge | Connection) => setEdges((els) => addEdge(params, els)),
-    [setEdges],
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [],
   );
 
   return (
@@ -55,13 +21,12 @@ const BasicFlow = () => {
         onNodesChange={onNodesChange}
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
-        nodeTypes={nodeTypes}
+        snapToGrid={true}
         fitView
-      >
-        <Background />
-      </ReactFlow>
+        attributionPosition="top-right"
+      />
     </div>
   );
 };
 
-export default BasicFlow;
+export default TestFlow;
