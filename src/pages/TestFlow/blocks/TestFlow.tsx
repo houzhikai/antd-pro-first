e40135d2037e9bbabd48c3f3bd3c5adf306404dc
hzk <1562206787@qq.com>
2023-06-08 20:47:18 +0800

    fix: remove file content, introduce entrance file

--- src/pages/TestFlow/blocks/TestFlow.tsx
+++ src/pages/TestFlow/blocks/TestFlow.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { useCallback } from 'react';
+import ReactFlow, { useNodesState, useEdgesState, addEdge } from 'reactflow';
+
+import initialNodes from './components/nodes';
+import initialEdges from './components/edges';
+
+import 'reactflow/dist/style.css';
 
 const TestFlow = () => {
-  return <div>TestFlow</div>;
+  const [nodes, , onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const onConnect = useCallback(
+    (params) => setEdges((eds) => addEdge(params, eds)),
+    [],
+  );
+
+  return (
+    <div style={{ height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        snapToGrid={true}
+        fitView
+        attributionPosition="top-right"
+      />
+    </div>
+  );
 };
 
 export default TestFlow;
--- src/pages/TestFlow/index.tsx
+++ src/pages/TestFlow/index.tsx
@@ -1,32 +1,8 @@
-import React, { useCallback } from 'react';
-import ReactFlow, { useNodesState, useEdgesState, addEdge } from 'reactflow';
+import TestFlow from './blocks/TestFlow';
 import 'reactflow/dist/style.css';
 
-import initialNodes from './blocks/components/nodes';
-import initialEdges from './blocks/components/edges';
-
-const TestFlow = () => {
-  const [nodes, , onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    [],
-  );
-
-  return (
-    <div style={{ height: '100vh' }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        snapToGrid={true}
-        fitView
-        attributionPosition="top-right"
-      />
-    </div>
-  );
+const TestFlowPage = () => {
+  return <TestFlow />;
 };
 
-export default TestFlow;
+export default TestFlowPage;
