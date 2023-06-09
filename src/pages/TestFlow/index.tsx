6f3cf1348664e8a0e181d4531a1c77661183d3c9
hzk <1562206787@qq.com>
2023-06-09 14:41:15 +0800

    fix: demo2 test

--- src/pages/TestFlow/index.tsx
+++ src/pages/TestFlow/index.tsx
@@ -1,8 +1,14 @@
-import TestFlow from './blocks/TestFlow';
 import 'reactflow/dist/style.css';
+// import TestFlow from './blocks/TestFlow';
+import Demo2 from './blocks/Demo2';
 
 const TestFlowPage = () => {
-  return <TestFlow />;
+  return (
+    <div style={{ height: '100vh' }}>
+      {/* <TestFlow /> */}
+      <Demo2 />
+    </div>
+  );
 };
 
 export default TestFlowPage;
