3801f3a40d31d7ca9416bdb63b88cb33f804b45c
hzk <1562206787@qq.com>
2023-06-30 17:50:34 +0800

    fix: 添加logo

--- src/pages/Home/blocks/Home.tsx
+++ src/pages/Home/blocks/Home.tsx
@@ -7,11 +7,26 @@ const Home = () => {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', height: '90%' }}>
       <div
-        style={{ padding: '16px 10px 30px', background: '#fff', width: 1070 }}
+        style={{
+          padding: '16px 10px 30px',
+          background: '#fff',
+          width: 1070,
+        }}
       >
         <Image
-          style={{ width: 1050 }}
+          style={{ width: 1050, position: 'relative' }}
           src="http://www.sandtekcorp.com.cn/sandtekc/templates/sandtek/Banner.png"
+          preview={false}
+        />
+        <Image
+          style={{
+            position: 'absolute',
+            width: 349,
+            height: 79,
+            right: 655,
+            top: -40,
+          }}
+          src="http://www.sandtekcorp.com.cn/sandtekc/images/logo_blue_s.png"
           preview={false}
         />
         <div
