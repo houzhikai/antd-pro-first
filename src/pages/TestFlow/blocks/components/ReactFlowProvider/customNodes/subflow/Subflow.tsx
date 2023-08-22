2922acd3683a5995e14a06693a04cd2dc5172ed7
hzk <1562206787@qq.com>
2023-08-22 15:33:24 +0800

    添加 subflow 的示例demo

--- src/pages/TestFlow/blocks/components/ReactFlowProvider/customNodes/subflow/Subflow.tsx
+++ src/pages/TestFlow/blocks/components/ReactFlowProvider/customNodes/subflow/Subflow.tsx
@@ -1,36 +1,21 @@
-import { Handle, Position } from 'reactflow';
-import styles from './index.less';
+import { useModel } from '@umijs/max';
+import { subflowNode } from './node';
+import { subflowEdge } from './edges';
+import { useEffect } from 'react';
 
-const Subflow = ({ data }) => {
-  return (
-    <div className={styles.wrapper1}>
-      <div className={styles.name}>{data.label}-11</div>
-      <Handle
-        type="target"
-        className={styles.top}
-        position={Position.Top}
-        id="a"
-      />
-      <Handle
-        type="target"
-        className={styles.left}
-        position={Position.Left}
-        id="b"
-      />
-      <Handle
-        type="target"
-        className={styles.bottom}
-        position={Position.Bottom}
-        id="c"
-      />
-      <Handle
-        type="target"
-        className={styles.right}
-        position={Position.Right}
-        id="d"
-      />
-    </div>
-  );
+const Subflow = () => {
+  let { nodes, setNodes, edges, setEdges } = useModel('useTestFlowModel');
+  let newNodes: any = [];
+  let newEdges: any = [];
+  useEffect(() => {
+    newNodes = nodes.concat(subflowNode);
+    newEdges = edges.concat(subflowEdge);
+    // console.log({ nodes, subflowNode, subflowEdge, newNodes });
+    setNodes(newNodes);
+    setEdges(newEdges);
+  }, []);
+
+  return <div></div>;
 };
 
 export default Subflow;
