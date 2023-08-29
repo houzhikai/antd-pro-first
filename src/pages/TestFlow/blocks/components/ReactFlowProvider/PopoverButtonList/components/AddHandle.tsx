cd64f91568a7ee118b15065ab8afb32e73a378b0
hzk <1562206787@qq.com>
2023-08-29 15:38:23 +0800

    fix: 添加节点时区分source/target

--- src/pages/TestFlow/blocks/components/ReactFlowProvider/PopoverButtonList/components/AddHandle.tsx
+++ src/pages/TestFlow/blocks/components/ReactFlowProvider/PopoverButtonList/components/AddHandle.tsx
@@ -5,12 +5,7 @@ import styles from '../index.less';
 
 const AddHandle = () => {
   const { setHandleList, handleList } = useModel('useTestFlowModel');
-  const buttonList = [
-    { type: 'source', className: 'top', position: 'Top', id: '0' },
-    { type: 'target', className: 'left', position: 'Left', id: '1' },
-    { type: 'source', className: 'bottom', position: 'Bottom', id: '2' },
-    { type: 'source', className: 'right', position: 'Right', id: '3' },
-  ];
+
   //   已经存在的port被禁用，不可再次选中
   const positionList = handleList.map((item) => item.position);
 
@@ -21,23 +16,54 @@ const AddHandle = () => {
     setHandleList((preList) => [...preList, item]);
   };
 
-  const list = (
+  const list: any = [
+    {
+      id: 0,
+      position: 'source',
+      buttonList: [
+        { type: 'source', className: 'top', position: 'Top', id: '0' },
+        { type: 'source', className: 'left', position: 'Left', id: '1' },
+        { type: 'source', className: 'bottom', position: 'Bottom', id: '2' },
+        { type: 'source', className: 'right', position: 'Right', id: '3' },
+      ],
+    },
+    {
+      id: 1,
+      position: 'target',
+      buttonList: [
+        { type: 'target', className: 'top', position: 'Top', id: '0' },
+        { type: 'target', className: 'left', position: 'Left', id: '1' },
+        { type: 'target', className: 'bottom', position: 'Bottom', id: '2' },
+        { type: 'target', className: 'right', position: 'Right', id: '3' },
+      ],
+    },
+  ];
+  const portList = (
     <>
-      {buttonList.map((item) => (
-        <Button
-          disabled={positionList.includes(item.position)}
-          key={item.className}
-          type="primary"
-          className={styles['bottom-item']}
-          onClick={() => handleAddHandle(item)}
+      {list.map((item) => (
+        <Popover
+          key={item.id}
+          placement="right"
+          trigger="hover"
+          content={item.buttonList.map((t) => (
+            <Button
+              disabled={positionList.includes(t.position)}
+              key={t.className}
+              type="primary"
+              className={styles['bottom-item']}
+              onClick={() => handleAddHandle(t)}
+            >
+              {t.position}
+            </Button>
+          ))}
         >
-          {item.position}
-        </Button>
+          <Button className={styles['bottom-item']}>{item.position}</Button>
+        </Popover>
       ))}
     </>
   );
   return (
-    <Popover placement="right" title="" content={list} trigger="hover">
+    <Popover placement="right" title="" content={portList} trigger="hover">
       <Button className={styles['bottom-item']}>添加测试项节点</Button>
     </Popover>
   );
