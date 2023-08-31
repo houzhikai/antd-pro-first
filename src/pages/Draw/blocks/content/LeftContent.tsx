b28ed19a76a0c5f4668abf0467038fba096b029f
hzk <1562206787@qq.com>
2023-08-31 13:55:07 +0800

    fix: 左边已有折叠面板样式

--- src/pages/Draw/blocks/content/LeftContent.tsx
+++ src/pages/Draw/blocks/content/LeftContent.tsx
@@ -1,7 +1,51 @@
+import { Collapse } from 'antd';
 import styles from '../index.less';
 
+const { Panel } = Collapse;
+
 const LeftContent = () => {
-  return <div className={styles.left}>LeftContent</div>;
+  const text = `
+A dog is a type of domesticated animal.
+Known for its loyalty and faithfulness,
+it can be found as a welcome guest in many households across the world.
+`;
+
+  return (
+    <div className={styles.left}>
+      <Collapse defaultActiveKey={['hardBin']} ghost>
+        <Panel
+          header={<div className={styles.header}>hardBin</div>}
+          key="hardBin"
+        >
+          {text}
+        </Panel>
+        <Panel
+          header={<div className={styles.header}>softBin</div>}
+          key="softBin"
+        >
+          {text}
+        </Panel>
+        <Panel
+          header={<div className={styles.header}>测试项</div>}
+          key="test-item"
+        >
+          {text}
+        </Panel>
+        <Panel
+          header={<div className={styles.header}>subflow</div>}
+          key="subflow"
+        >
+          {text}
+        </Panel>
+        <Panel
+          header={<div className={styles.header}>mainFlow</div>}
+          key="mainFlow"
+        >
+          {text}
+        </Panel>
+      </Collapse>
+    </div>
+  );
 };
 
 export default LeftContent;
