83d2fc9d6c8cbf099b3c3469deb19994807a12aa
hzk <1562206787@qq.com>
2023-10-20 17:48:55 +0800

    fix: save mainflow flow.json finish

--- src/pages/Draw/blocks/NavAction/Save.tsx
+++ src/pages/Draw/blocks/NavAction/Save.tsx
@@ -3,28 +3,33 @@ import { Button, Tooltip } from 'antd';
 // import save from '@/icon/draw/save.svg';
 
 import styles from '../index.less';
+import saveFlow from '@/components/saveFlow';
+import analyzeFlow from '@/components/analyzeFlow';
 
 const Save = () => {
-  const { title, mergeArrList, nodes, edges } = useModel('useTestFlowModel');
+  // const { title, mergeArrList, nodes, edges } = useModel('useTestFlowModel');
+  const { flows } = useModel('useDrawModel');
 
   const handleSave = () => {
     // ` `换行也可以使console表示换行
-    const startNote = [
-      `et::MainFlow('${title}'), true,
-    `,
-    ];
+    const xxx = saveFlow(analyzeFlow(flows));
+    console.log('给后端的flow.json内容', xxx);
+    // const startNote = [
+    //   `et::MainFlow('${title}'), true,
+    // `,
+    // ];
 
-    const outputTestFlowList = mergeArrList.map(
-      (item: any, index) =>
-        `et::Unit<${item.source}>('${item.source}'${
-          index === 0 ? ', true' : ''
-        }, ${item.child.map((p, idx) => `et::Port(${idx}, ${p.target}),`)}
-          `,
-    );
+    // const outputTestFlowList = mergeArrList.map(
+    //   (item: any, index) =>
+    //     `et::Unit<${item.source}>('${item.source}'${
+    //       index === 0 ? ', true' : ''
+    //     }, ${item.child.map((p, idx) => `et::Port(${idx}, ${p.target}),`)}
+    //       `,
+    // );
 
-    const outputTestFlowBuilder = startNote.concat(outputTestFlowList).join('');
-    console.log('mergeArrList=======', mergeArrList, nodes, edges);
-    console.log('outputTestFlowBuilder=======', outputTestFlowBuilder);
+    // const outputTestFlowBuilder = startNote.concat(outputTestFlowList).join('');
+    // console.log('mergeArrList=======', mergeArrList, nodes, edges);
+    // console.log('outputTestFlowBuilder=======', outputTestFlowBuilder);
   };
 
   return (
