12420ac7ac55fcf88431fda2c1ab6609722eee43
hzk <1562206787@qq.com>
2023-10-21 11:22:37 +0800

    fix: 合并 保存时多余代码，

--- src/components/saveFlow/index.tsx
+++ src/components/saveFlow/index.tsx
@@ -6,104 +6,70 @@ const saveFlow = (flows) => {
     (item) => !item.id.includes('fen-bin'),
   );
 
-  const mainflowPorts = (name) => {
-    const filterSameName = flows.mainFlowEdges.filter((item) => {
-      const nodeIds = item.id.split('->');
-      const sourceNode = nodeIds[0];
-      return sourceNode === name;
-    });
-    //  "param": 0, "type": "4",  "value": "FB1"
+  const getFlowList = (flowParams, filterFBinList, edges) => {
+    const mainflowPorts = (name) => {
+      const filterSameName = edges.filter((item) => {
+        const nodeIds = item.id.split('->');
+        const sourceNode = nodeIds[0];
+        return sourceNode === name;
+      });
+      //  "param": 0, "type": "4",  "value": "FB1"
 
-    return filterSameName.map((item) => {
-      const nodeIds = item.id.split('->');
-      const targetNode = nodeIds[1];
-      let type;
-      if (item.target.includes('test-method')) {
-        type = '1';
-      } else if (item.target.includes('subflow')) {
-        type = '2';
-      } else {
-        type = '4';
-      }
+      return filterSameName.map((item) => {
+        const nodeIds = item.id.split('->');
+        const targetNode = nodeIds[1];
+        let type;
+        if (item.target.includes('test-method')) {
+          type = '1';
+        } else if (item.target.includes('subflow')) {
+          type = '2';
+        } else {
+          type = '4';
+        }
+        return {
+          param: Number(item.label),
+          type: type,
+          value: targetNode,
+        };
+      });
+    };
+    const nodesList = filterFBinList.map((item) => {
+      const params = item.params;
       return {
-        param: Number(item.label),
-        type: type,
-        value: targetNode,
+        isFlowUnit: params.isFlowUnit,
+        isStartUnit: params.isStartUnit,
+        name: params.name,
+        number: params.number,
+        targetFlowName: params.targetFlowName,
+        testMethod: params.testMethod,
+        variables: params.variables,
+        ports: mainflowPorts(params.name),
       };
     });
+    const mainflowList = flowParams.map((item) => {
+      return {
+        flowName: item.flowName,
+        globalVariables: item.globalVariables,
+        isActive: item.isActive,
+        isMain: item.isMain,
+        units: nodesList,
+      };
+    });
+    return mainflowList;
   };
-  const nodesList = filterFenBinNodeList.map((item) => {
-    const params = item.params;
-    return {
-      isFlowUnit: params.isFlowUnit,
-      isStartUnit: params.isStartUnit,
-      name: params.name,
-      number: params.number,
-      targetFlowName: params.targetFlowName,
-      testMethod: params.testMethod,
-      variables: params.variables,
-      ports: mainflowPorts(params.name),
-    };
-  });
-  const mainflowList = flows.mainFlowParams.map((item) => {
-    return {
-      flowName: item.flowName,
-      globalVariables: item.globalVariables,
-      isActive: item.isActive,
-      isMain: item.isMain,
-      units: nodesList,
-    };
-  });
+
+  const mainflowList = getFlowList(
+    flows.mainFlowParams,
+    filterFenBinNodeList,
+    flows.mainFlowEdges,
+  );
 
   // subflow
-  const subflowPorts = (name) => {
-    const filterSameName = flows.subFlowEdges.filter((item) => {
-      const nodeIds = item.id.split('->');
-      const sourceNode = nodeIds[0];
-      return sourceNode === name;
-    });
-    //  "param": 0, "type": "4",  "value": "FB1"
-
-    return filterSameName.map((item) => {
-      const nodeIds = item.id.split('->');
-      const targetNode = nodeIds[1];
-      let type;
-      if (item.target.includes('test-method')) {
-        type = '1';
-      } else if (item.target.includes('subflow')) {
-        type = '2';
-      } else {
-        type = '4';
-      }
-      return {
-        param: Number(item.label),
-        type: type,
-        value: targetNode,
-      };
-    });
-  };
-  const subNodesList = filterSubFenBinNodeList.map((item) => {
-    const params = item.params;
-    return {
-      isFlowUnit: params.isFlowUnit,
-      isStartUnit: params.isStartUnit,
-      name: params.name,
-      number: params.number,
-      targetFlowName: params.targetFlowName,
-      testMethod: params.testMethod,
-      variables: params.variables,
-      ports: subflowPorts(params.name),
-    };
-  });
-  const subflowList = flows.subFlowParams.map((item) => {
-    return {
-      flowName: item.flowName,
-      globalVariables: item.globalVariables,
-      isActive: item.isActive,
-      isMain: item.isMain,
-      units: subNodesList,
-    };
-  });
+  const subflowList = getFlowList(
+    flows.subFlowParams,
+    filterSubFenBinNodeList,
+    flows.subFlowEdges,
+  );
 
   return { flows: { testFlows: [...mainflowList, ...subflowList] } };
 };
--- src/components/saveFlow/oldFile.tsx
+++ src/components/saveFlow/oldFile.tsx
@@ -2,6 +2,9 @@ const saveFlow = (flows) => {
   const filterFenBinNodeList = flows.mainFlowNodes.filter(
     (item) => !item.id.includes('fen-bin'),
   );
+  const filterSubFenBinNodeList = flows.subFlowNodes.filter(
+    (item) => !item.id.includes('fen-bin'),
+  );
 
   const mainflowPorts = (name) => {
     const filterSameName = flows.mainFlowEdges.filter((item) => {
@@ -51,6 +54,57 @@ const saveFlow = (flows) => {
       units: nodesList,
     };
   });
-  return { flows: { testFlows: mainflowList } };
+
+  // subflow
+  const subflowPorts = (name) => {
+    const filterSameName = flows.subFlowEdges.filter((item) => {
+      const nodeIds = item.id.split('->');
+      const sourceNode = nodeIds[0];
+      return sourceNode === name;
+    });
+    //  "param": 0, "type": "4",  "value": "FB1"
+
+    return filterSameName.map((item) => {
+      const nodeIds = item.id.split('->');
+      const targetNode = nodeIds[1];
+      let type;
+      if (item.target.includes('test-method')) {
+        type = '1';
+      } else if (item.target.includes('subflow')) {
+        type = '2';
+      } else {
+        type = '4';
+      }
+      return {
+        param: Number(item.label),
+        type: type,
+        value: targetNode,
+      };
+    });
+  };
+  const subNodesList = filterSubFenBinNodeList.map((item) => {
+    const params = item.params;
+    return {
+      isFlowUnit: params.isFlowUnit,
+      isStartUnit: params.isStartUnit,
+      name: params.name,
+      number: params.number,
+      targetFlowName: params.targetFlowName,
+      testMethod: params.testMethod,
+      variables: params.variables,
+      ports: subflowPorts(params.name),
+    };
+  });
+  const subflowList = flows.subFlowParams.map((item) => {
+    return {
+      flowName: item.flowName,
+      globalVariables: item.globalVariables,
+      isActive: item.isActive,
+      isMain: item.isMain,
+      units: subNodesList,
+    };
+  });
+
+  return { flows: { testFlows: [...mainflowList, ...subflowList] } };
 };
 export default saveFlow;
